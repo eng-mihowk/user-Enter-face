66dd086f3844e02183dc5a4d8eaac3689df56268
abdullah jameel al-thomali <66957695+eng-mihowk@users.noreply.github.com>
2021-06-21 21:55:27 +0300

    Add files via upload

---
+++ servo.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 17 يونيو 2021 الساعة 17:50
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `servodegree`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `servo`
+--
+
+CREATE TABLE `servo` (
+  `servo1` int(165) NOT NULL,
+  `servo2` int(165) NOT NULL,
+  `servo3` int(165) NOT NULL,
+  `servo4` int(165) NOT NULL,
+  `servo5` int(165) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
